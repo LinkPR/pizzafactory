6c8b2e1161820b3a4133ebe4487a7eb25d675481
ricardoaponte <ricardo@rayvenues.com>
2018-03-10 13:05:06 -0400

    Create flow.sol
    Contract development guide for pizza factory

---
+++ contracts/flow.sol
@@ -0,0 +1,43 @@
+// Specify solidity compiler version
+
+// Create contract
+    
+    // Define owner
+    
+    // Pizza information
+
+    // Save Amount Paid
+    
+    // Users who will pay for the pizza
+    
+    // Initialize Contract
+    
+        // Save the contract owner
+
+        // Set pizza information
+
+    // Pay for Pizza
+    
+        // Do not allow contract owner to pay for pizza
+        
+        // Validate that the payment is more than 0
+        
+        // Save payment
+        
+        // Return a success message
+    
+    // Make Pizza
+    
+        // Check if totalAmountPaid = pizzaPrice
+        
+            // Pay owner
+
+            // Return success message
+
+
+            // Otherwise return fail message
+    
+    // Challenge Round!
+
+    // Get amount pending 
+    // Get User Payment Amount
